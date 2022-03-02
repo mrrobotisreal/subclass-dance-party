a224490bc4ffb98d5ab367de7fd05d0928ad2d3e
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-03-01 19:55:34 -0700

    Figured out git and pulled our files to my machine from Benjamins repository

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -17,6 +17,25 @@ describe('blinkyDancer', function() {
     blinkyDancer.step();
     expect(blinkyDancer.$node.toggle.called).to.be.true;
   });
+
+  //-------------------------------------------------------
+
+  var jQueryShow = sinon.stub($.fn, 'show');
+  var jQueryHide = sinon.stub($.fn, 'hide');
+
+  jQueryShow.callCount.should.be.equal(1);
+  jQueryShow.thisValues[0].selector.should.be.equal("#login");
+
+  jQueryHide.callCount.should.be.equal(1);
+  jQueryHide.thisValues[0].selector.should.be.equal("#logout");
+
+  it('should have a grow function that makes it grow', function() {
+    sinon.spy(growingDancer.$node, 'on'); // or 'css'
+    growingDancer.grow();
+    expect(growingDancer.$node.on.called).to.be.true;
+  });
+
+  //-------------------------------------------------------
 
   describe('dance', function() {
     it('should call step at least once per second', function() {
